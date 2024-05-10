9f2b7ff8c72d53da6c8e497a0ae6572fbda5d89a
arismoraitis <105006025+arismoraitis@users.noreply.github.com>
2024-05-10 04:15:58 +0300

    Update reveal-fade.js

--- reveal-fade.js
+++ reveal-fade.js
@@ -20,3 +20,17 @@ window.addEventListener("scroll", reveal);
 // ]]></script>
 */
 /* kodikas gia animation */
+
+<script>// <![CDATA[
+//Get the button
+
+document.getElementById("scrollToTopBtn").addEventListener("click", topFunction);
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+        window.scrollBy(0, -50);
+        requestAnimationFrame(topFunction);
+    }
+}
+// ]]></script>
